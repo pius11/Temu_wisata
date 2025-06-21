c41c08a2e7340103fe562918677fef09bb970bce
DanielWidhi <danielwidhi98@gmail.com>
2025-06-21 19:41:18 +0800

    mysql

--- temuapp/api/urls.py
+++ temuapp/api/urls.py
@@ -6,9 +6,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views.postTouristSpots import create_tourist_spot
 from .views.getAllSpots import list_tourist_spots, count_verified_spots
 from .views.updateUser import update_current_user
-from .views.postReview import create_review
-from .views.getAllReviews import list_reviews
-from .views.getReviewsBySpot import get_reviews_by_spot
+# from .views.postReview import create_review
+# from .views.getAllReviews import list_reviews
+# from .views.getReviewsBySpot import get_reviews_by_spot
 from .views.getCurrentUser import get_current_user
 from .views.ai import chat_ai
 from .views.updateSpot import update_tourist_spot
@@ -30,9 +30,9 @@ urlpatterns = [
     path('touristspots/all/', list_tourist_spots, name='list-tourist-spots'),
     path('touristspots/count-verified/', count_verified_spots, name='count-verified-spots'),
     path('users/me/update/', update_current_user, name='update-user'),
-    path('reviews/', create_review, name='create-review'),
-    path('reviews/all/', list_reviews, name='list-reviews'),
-    path('reviews/spot/<int:spot_id>/', get_reviews_by_spot, name='get-reviews-by-spot'),
+    # path('reviews/', create_review, name='create-review'),
+    # path('reviews/all/', list_reviews, name='list-reviews'),
+    # path('reviews/spot/<int:spot_id>/', get_reviews_by_spot, name='get-reviews-by-spot'),
     path('users/me/', get_current_user, name='get-current-user'),
     path('chat/', chat_ai, name='chat-ai'),
     path('touristspots/<int:pk>/update/', update_tourist_spot, name='update-tourist-spot'),
