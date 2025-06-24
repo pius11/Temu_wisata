1d854485b33a91e23afa8cd7f0596d4af3e1e743
qiura122 <emowijaya122@gmail.com>
2025-06-24 08:12:03 +0800

    search

---
+++ temuapp/api/views/search.py
@@ -0,0 +1,14 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from temuapp.models import TouristSpot
+from temuapp.api.serializers.touristSpotsSerializ import TouristSpotSerializer
+
+@api_view(['GET'])
+def search_tourist_spot(request):
+    query = request.GET.get('q', '')
+    if query:
+        spots = TouristSpot.objects.filter(name__icontains=query, is_removed=False)
+    else:
+        spots = TouristSpot.objects.none()
+    serializer = TouristSpotSerializer(spots, many=True)
+    return Response(serializer.data)
